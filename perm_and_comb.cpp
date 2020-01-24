1ab99b2286488d4c0457c4f748242c6dd7cfaaa5
praveenkumar <praveenkumar06072002@gmail.com>
2020-01-24 16:17:24 +0530

    assignment completed

--- perm_and_comb.cpp
+++ perm_and_comb.cpp
@@ -3,15 +3,59 @@ using namespace std;
 
 int permutation(int n, int r)
 {
-  return 0;
+	
+	if(r>1)
+	return n*permutation(n-1,r-1);
+	if(r==1)
+	return n;
+	
 }
 
 int combination(int n, int r)
 {
-  return 0;
+	if(r>1)
+	{
+		return n*combination(n-1,r-1)/r;
+	}
+	else if(r==1)
+	{
+		return n;
+	}
+
 }
 
 int main()
 {
+	int choice;
+	int n=0,r=0;
+	cout<<" ENTER 1 TO FIND PERMUTATION "<<endl;
+	cout<<" ENTER 2 TO FIND COMBINATION "<<endl;
+	cout<<" enter your choice :: ";
+	cin>>choice;
+	cout<<endl;
+	cout<<" enter value of n: ";
+	cin>>n;
+	cout<<" \n enter value of r : ";
+	cin>>r;
+	try
+	{
+		if(n<0||r<0)
+		throw " 'n' and 'r' can not be negative ";
+	}
+	catch(char const  *str)
+	{
+		cout<<str;
+		exit(1);
+	}
+	if(choice==1)
+	{
+		cout<<" permutation will be : "<<permutation(n,r);
+	}
+	else if(choice==2)
+	{
+		cout<<" combination will be : "<<combination(n,r);
+	}
+	else
+	cout<<" WRONG CHOICE ";
   return 0;
 }
